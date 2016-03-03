72329026fbbee8feb6a9df39c60d221786de45b7
lackinginflavor <lackinginflavor@users.noreply.github.com>
2016-03-03 16:22:26 -0500

    working on user entered values

--- 24game.py
+++ 24game.py
@@ -2,6 +2,26 @@ import random
 
 playNumbers = []
 for x in range(4):
-    playNumbers.append(random.randint(1,9))
+    playNumbers.append(random.randint(1,9) + 0.0)
 
+print "The object of this game is to take the below numbers and make them equal 24."
 print playNumbers
+print "You can use any +, -, *, / and ()"
+userInput = raw_input('Please enter your expression now:')
+
+userList = []
+for s in userInput:
+    userList.append(s)
+
+print userList
+
+for r in userList:
+    if r == ' ':
+        userList.remove(' ') #trying to remove spaces
+    elif r.isdigit():
+        r = float(r) #trying to  convert '#s' to floats
+    elif r != '*' or r != '/' or r != '+' or r != '-' or r != '(' or r != ')':
+        userList.remove(r)
+        print "You had an invalid entry"
+
+print userList
